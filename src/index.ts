5c147d8a46509b4bb7aa3bdea75b938b5d651ef0
shangxue <1919892171@qq.com>
2025-03-24 18:45:09 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
-import { Context, Schema, Session } from "koishi";
-import type { OneBotBot } from "koishi-plugin-adapter-onebot";
+import { Context, Schema, h } from "koishi";
 
 export const usage = `
   <h2>请先部署 <a href="https://github.com/LingLambda/JMComic-Api">JMComic-Api</a></h2>
@@ -19,37 +18,28 @@ export const Config: Schema<Config> = Schema.object({
 });
 
 export function apply(ctx: Context, config: Config) {
-  ctx
-    .command("jmid <id>", "获取指定id jm漫画")
+  ctx.command("jmid <id>", "获取指定id jm漫画")
+    .example("jmid 422866 获取422866编号的jm漫画")
     .action(async ({ session }, id) => {
-      getJM(ctx, config, session, id);
+      if (session.platform !== "onebot") {
+        await session.send("暂仅支持onebot适配器喵");
+        return;
+      }
+      if (!id) {
+        await session.send('未提供id喵')
+        return;
+      }
+
+      const url = config.url;
+      ctx.http.get(`${url}/get_pdf_path/${id}`).then(async (res: any) => {
+        console.log(res)
+        if (res.success == true) {
+          const { data: pdf_path } = res;
+          await session.send(h.file(pdf_path))
+          return
+        } else {
+          await session.send("后台发生错误");
+        }
+      });
     })
-    .example("jmid 422866 获取422866编号的jm漫画");
 }
-
-const getJM = async (
-  ctx: Context,
-  config: Config,
-  session: Session,
-  id: string
-) => {
-  if (!session.onebot) {
-    session.send("暂仅支持onebot适配器喵");
-    return;
-  }
-  if(!id){
-    session.send('未提供id喵')
-    return;
-  }
-
-  const url = config.url;
-  ctx.http.get(`${url}/get_pdf_path/${id}`).then((res: any) => {
-    console.log(res)
-    if (res.success == true) {
-      const { data: pdf_path, name } = res;
-      session.onebot.uploadGroupFile(session.onebot.group_id, pdf_path,name);
-    } else {
-      session.send("后台发生错误");
-    }
-  });
-};
